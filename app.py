8b9084daf917dfcded43162ee322b80fb2528618
Dim-Dim-git <dmitry.cgwork@gmail.com>
2025-12-05 19:42:13 +0000

    Добавил базовые шаблоны и стили

--- app.py
+++ app.py
@@ -2,22 +2,26 @@
 Сайт Gleeful.ru - Агентство праздников
 """
 
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template
 
 app = Flask(__name__)
-
 app.config['SECRET_KEY'] = '7a3f9e2c8b4d1f6e5a9c3b7d2f8e4a1b6c5d9f3e7a2b8c4d1f6e5a9c3b7d2f8e'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///party_agency.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
-    return '<h1>Gleeful - Твоя территория радости!</h1><p>Сайт в разработке...</p>'
+    return render_template('index.html')
+
+@app.route('/services')
+def services():
+    return '<h1>Каталог услуг</h1><p>Скоро здесь появится список наших услуг...</p>'
+
+@app.route('/about')
+def about():
+    return '<h1>О нас</h1><p>Информация о компании Gleeful</p>'
+
+@app.route('/contacts')
+def contacts():
+    return '<h1>Контакты</h1><p>Свяжитесь с нами</p>'
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
     app.run(debug=True)
